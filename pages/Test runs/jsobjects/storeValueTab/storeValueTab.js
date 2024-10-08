c91b5f5559ca6982f3f0f4cacc1b2b7b684e71ac
raksha <raksha@appsmith.com>
2024-10-08 08:24:15 +0000

    System generated commit, initial commit

---
+++ pages/Test runs/jsobjects/storeValueTab/storeValueTab.js
@@ -0,0 +1,15 @@
+export default {
+	// myVar1: ["Tab1", "Tab2"],
+	myVar2: {},
+	myFun1 (a="Tab 1") {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		
+		return storeValue('tab', a);
+		
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
