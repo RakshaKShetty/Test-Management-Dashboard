2340e6d504009fc60183b76a22a9613df77f72b8
kamakshi <kamakshi@appsmith.com>
2024-10-08 12:04:07 +0000

    add a new page for test run details using query params

---
+++ pages/Test run details/jsobjects/TestRunManagement/TestRunManagement.js
@@ -0,0 +1,64 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	
+	/*async insertTR () {
+		
+	let  currentID = 	await Insert_TRs.run()
+	const a =  currentID[0].run_id ;
+	
+	const selectedTC = (TestCase.selectedRows)
+	 for (let i = 0 ; i < selectedTC.length ; i++){
+		 
+		 {
+			InsertTCinTR.run({runID: a , caseID:selectedTC[i].case_id }) 
+		 }
+	 
+	
+	}
+	},*/
+	async updateTCsinTR () {
+		
+		const updatedTC = (Table1.updatedRows)
+		
+	//return updatedTC[0].allFields
+		for (let i=0 ; i < updatedTC.length ;i++)
+			
+			{
+				UpdateTestRunCases.run({tcstatus:updatedTC[i].allFields.tc_status , trid: updatedTC[i].allFields.trc_id})
+			} 
+		
+	},
+	async getTRprogess (runid=appsmith.URL.queryParams.trid)
+	{
+	 //const passcount =  await Total_Passed_cases_in_a_run.run(runid)
+	await FetchTCsforIndividualTR.run()
+	 	const totalcount = FetchTCsforIndividualTR.data.length
+		
+	await Total_Passed_cases_in_a_run.run({runid})
+		 const passedcount= Total_Passed_cases_in_a_run.data[0].passedcount
+		 
+		 
+		 const progress = (passedcount/totalcount )*100
+		 
+		 if (progress > 0 )
+			 return Math.floor(progress)
+		
+		else return 0
+		 
+	  
+	},
+	async getTRprogess1 (runid = '26')
+	{
+	 //const passcount =  await Total_Passed_cases_in_a_run.run(runid)
+	//await FetchTCsforIndividualTR.run()
+	/// 	const totalcount = FetchTCsforIndividualTR.data.length
+		
+	return await Total_Passed_cases_in_a_run.run({runid})
+		 //const passedcount= Total_Passed_cases_in_a_run.data[0].passedcount
+		// return passedcount
+		 //return (passedcount/totalcount )*100
+		 
+	  
+	}
+}
