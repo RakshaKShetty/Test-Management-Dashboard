8dec4ac10ab0a52c00738918466838fffb6f04d5
kamakshi <kamakshi@appsmith.com>
2024-10-15 19:52:10 +0000

    change with respect to TCs

---
+++ pages/Test runs/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,32 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+
+	async	queryTransform(a = FilterTags.selectedOptionLabels) {
+
+		if (a.length == 0 )
+
+		{		return "'0'"
+
+		 FetchTC.run()
+		}
+
+		else 
+		{
+
+			const originalArray = a
+			const stringWithSingleQuotes = originalArray.map(item => `'${item}'`).join(', ')
+			return stringWithSingleQuotes 
+
+
+
+		}
+	},
+	async fetchAllCases()
+	{
+		await this.queryTransform()
+		const allCases = await FetchTC.run({limit: data_table.pageSize , offset : data_table.pageOffset} )
+
+		return allCases
+	}
+}
