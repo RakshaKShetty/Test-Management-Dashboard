3b9c2b44698937d6cce2bf5c4de01a62b3ce7a0b
kamakshi <kamakshi@appsmith.com>
2024-10-16 08:39:24 +0000

    commit changes in progress percentage

--- pages/Test run details/jsobjects/TestRunManagement/TestRunManagement.js
+++ pages/Test run details/jsobjects/TestRunManagement/TestRunManagement.js
@@ -1,64 +1,70 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	
+
 	/*async insertTR () {
-		
+
 	let  currentID = 	await Insert_TRs.run()
 	const a =  currentID[0].run_id ;
-	
+
 	const selectedTC = (TestCase.selectedRows)
 	 for (let i = 0 ; i < selectedTC.length ; i++){
-		 
+
 		 {
 			InsertTCinTR.run({runID: a , caseID:selectedTC[i].case_id }) 
 		 }
-	 
-	
+
+
 	}
 	},*/
 	async updateTCsinTR () {
-		
+
 		const updatedTC = (Table1.updatedRows)
-		
-	//return updatedTC[0].allFields
+
+		//return updatedTC[0].allFields
 		for (let i=0 ; i < updatedTC.length ;i++)
-			
-			{
-				UpdateTestRunCases.run({tcstatus:updatedTC[i].allFields.tc_status , trid: updatedTC[i].allFields.trc_id})
-			} 
-		
+
+		{
+			UpdateTestRunCases.run({tcstatus:updatedTC[i].allFields.tc_status , trid: updatedTC[i].allFields.trc_id})
+		} 
+
 	},
-	async getTRprogess (runid=appsmith.URL.queryParams.trid)
+	async getTRprogress2(runid = appsmith.URL.queryParams.trid) {
+  // Fetch total number of test cases for the given test run
+  await FetchTCsforIndividualTR.run();
+
+  // Get total number of test cases
+  const totalcount = FetchTCsforIndividualTR.data.length;
+
+  // Fetch the passed test cases data for the given runid
+  const data = await Total_Passed_cases_in_a_run.run({ runid });
+
+  // Handle the case where no passed cases are returned
+  if (!data || data.length === 0 || data[0].passedcount === null) {
+    return 0;
+  } else {
+    // Extract passed count
+    const passedcount = data[0].passedcount;
+
+    // Calculate progress percentage
+    const progress = (passedcount / totalcount) * 100;
+
+    // Return the floored progress if greater than 0, otherwise 0
+    return progress > 0 ? Math.floor(progress) : 0;
+  }
+},
+	async getTRprogess1 (runid = '31')
 	{
-	 //const passcount =  await Total_Passed_cases_in_a_run.run(runid)
-	await FetchTCsforIndividualTR.run()
-	 	const totalcount = FetchTCsforIndividualTR.data.length
-		
-	await Total_Passed_cases_in_a_run.run({runid})
-		 const passedcount= Total_Passed_cases_in_a_run.data[0].passedcount
-		 
-		 
-		 const progress = (passedcount/totalcount )*100
-		 
-		 if (progress > 0 )
-			 return Math.floor(progress)
-		
-		else return 0
-		 
-	  
-	},
-	async getTRprogess1 (runid = '26')
-	{
-	 //const passcount =  await Total_Passed_cases_in_a_run.run(runid)
-	//await FetchTCsforIndividualTR.run()
-	/// 	const totalcount = FetchTCsforIndividualTR.data.length
-		
-	return await Total_Passed_cases_in_a_run.run({runid})
-		 //const passedcount= Total_Passed_cases_in_a_run.data[0].passedcount
+		//const passcount =  await Total_Passed_cases_in_a_run.run(runid)
+		//await FetchTCsforIndividualTR.run()
+		/// 	const totalcount = FetchTCsforIndividualTR.data.length
+
+		 await Total_Passed_cases_in_a_run.run({runid})
+		//const passedcount= Total_Passed_cases_in_a_run.data[0].passedcount
 		// return passedcount
-		 //return (passedcount/totalcount )*100
-		 
-	  
+		//return (passedcount/totalcount )*100
+		//Total_Passed_cases_in_a_run.data[0].passedcount
+
+
 	}
 }
